2c31be305c0c153c13f8755112a28b6dd0bfa090
sunsoo <sunsoo@wemakeprice.com>
2019-06-14 11:07:46 +0900

    react build test

---
+++ test/react_build/precache-manifest.ab35cc7004284e9d4ec1714588a20f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef24d58cf76f512c1c90b7a55251df6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e0d0faba022f74b845",
+    "url": "/static/css/main.7db66810.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e7e0d0faba022f74b845",
+    "url": "/static/js/main.22de85d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
